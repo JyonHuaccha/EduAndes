88e3beade7644010a48e5fd3080f67896bfb3652
Jyon Huaccha <yeferhuaccha@gmail.com>
2026-09-24 08:14:59 -0500

    primer commit

---
+++ src/main/java/pe/edu/EduAndes/EduAndesApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.EduAndes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduAndesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduAndesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/EduAndes/EduAndesApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.EduAndes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduAndesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
